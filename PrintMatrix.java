e63c0693db015dd08aebf5303d4da48e13ce6698
Pooja9586 <r.pooja9586@gmail.com>
2022-02-03 22:36:08 +0530

    print elements in matrix form
    takes user input for size and elements of matrix and prints in matrix format

---
+++ PrintMatrix.java
@@ -0,0 +1,30 @@
+package learning;
+
+import java.util.Scanner;
+
+public class PrintMatrix {
+
+	public static void main(String[] args) {
+		int m,n;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Add size of matrix");
+		System.out.println("Number of rows = ");
+		m = scanner.nextInt() ;
+		System.out.println("Number of columns = ");
+		n = scanner.nextInt() ;
+		System.out.println("Enter "+m*n+" elements for matrix");
+		String[][] arr = new String[m][n] ;
+		for(int i=0; i<m; i++)
+			for(int j=0;j<n;j++)
+				arr[i][j] = scanner.next() ;
+		
+		System.out.println("Matrix entered is ");
+		for(int i=0; i<m; i++) {
+			for(int j=0;j<n;j++) {
+				System.out.print(arr[i][j]+" ");
+			}
+			System.out.println();
+		}
+	}
+
+}
